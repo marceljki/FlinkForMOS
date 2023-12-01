fe32f8f1d76f403640418742a9e3da1f071f7f4c
Marcel Jankowski <marcel.jankowski@wps.de>
2023-12-01 12:12:29 +0100

    fix config issue

--- src/api/config.py
+++ src/api/config.py
@@ -1,11 +1,16 @@
 import configparser
+import os
 
 
 class Config:
 
     def __init__(self):
         self.config = configparser.ConfigParser()
-        self.config.read('api_keys.ini')
+        filename = '../api_keys.ini'
+        if os.path.isfile(filename):
+            self.config.read(filename)
+        else:
+            print("File \"%s\" does not exist" % filename)
 
     def get(self, name: str) -> str:
         return self.config.get('API_KEYS', name)
